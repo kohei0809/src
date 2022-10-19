949ae233758a03fa6d01bfbe5c448215f8b50b5b
kohei0809 <matsumotokohei0809@gmail.com>
2022-10-19 13:36:41 +0900

    real/exp2を2倍にした

--- agents/TargetPathAgent_TimeChange_Learning.java
+++ agents/TargetPathAgent_TimeChange_Learning.java
@@ -387,11 +387,11 @@ public class TargetPathAgent_TimeChange_Learning implements IAgent{
             rate = estimator.getLearnRate();
         }
         else{
-            rate = (1-ganma) * estimator.getLearnRate() + ganma * (realValue / exp2)*rate2;
+            rate = (1-ganma) * estimator.getLearnRate() + ganma * (realValue / exp2)*rate2*2;
             //rate = (1-ganma) * estimator.getLearnRate() + ganma * exp2;
             estimator.setLearnRate(rate);
         }
-        rateLogger.writeLine(time + "," + exp + "," + exp2 + "," + realValue + "," + rate2 + "," + orate + "," + rate + "," + moveCount + "," + zeroCount);
+        rateLogger.writeLine(time + "," + exp + "," + exp2 + "," + realValue + "," + (realValue/exp2) + "," + rate2 + "," + orate + "," + rate + "," + moveCount + "," + zeroCount);
      
         estimator.setLearnRate(rate);
         return;
